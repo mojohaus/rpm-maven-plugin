d5c6287f3db72b734b009d1d7d2f52949875ac95
Brett Okken <brett.okken.os@gmail.com>
2010-01-07 19:17:45 +0000

    add reactor build integration test
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/rpm-maven-plugin@11599 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/rpm-reactor/validate.groovy
@@ -0,0 +1,34 @@
+// $Id$
+import org.codehaus.mojo.unix.rpm.RpmUtil
+import org.codehaus.mojo.unix.rpm.RpmUtil.FileInfo
+import org.codehaus.mojo.unix.rpm.RpmUtil.SpecFile
+
+import java.io.*
+import java.util.List
+import java.util.Iterator
+
+
+File jar = new File((File) basedir, "rpm-jar/target/mojo-rpm-it-module-jar-1.0.jar")
+if (!jar.exists())
+    throw new java.lang.AssertionError("jar file does not exist");
+
+File war = new File((File) basedir, "rpm-war/target/mojo-rpm-it-module-war-1.0.war")
+if (!war.exists())
+    throw new java.lang.AssertionError("war file does not exist");
+
+File rpm = new File((File) basedir, "rpm-rpm/target/rpm/mojo-rpm-it-module-rpm/RPMS/noarch/mojo-rpm-it-module-rpm-1.0-1.noarch.rpm")
+if (!rpm.exists())
+    throw new java.lang.AssertionError("rpm file does not exist");
+
+SpecFile spec = RpmUtil.getSpecFileFromRpm(rpm)
+
+List fileInfos = RpmUtil.queryPackageForFileInfo(rpm)
+
+int fileCnt = fileInfos.size()
+System.out.println("File Count: " + fileCnt);
+System.out.println(fileInfos);
+
+if (fileCnt != 1)
+    throw new java.lang.AssertionError("number of files in rpm: " + fileCnt + "does not match expected: 1");
+
+return true
