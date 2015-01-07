454cc317f288280fe05643b41e1df93d66367abb
Dan Tran <dantran@gmail.com>
2015-01-07 04:22:51 +0000

    [MRPM-176] add groovy check when IT is enable
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/rpm-maven-plugin@20308 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/rpm-sign-with-keyname-lookup/verify.groovy
+++ src/it/rpm-sign-with-keyname-lookup/verify.groovy
@@ -1,3 +1,5 @@
 File buildLog = new File( basedir, "build.log" )
 assert buildLog.text.contains("keyname = Maven GPG Plugin (TESTING KEY) <dev@maven.apache.org>")
 assert buildLog.text.contains("passphraseServerKey = Maven GPG Plugin (TESTING KEY) <dev@maven.apache.org>")
+//assert buildLog.text.contains("[INFO] Pass phrase is good.")
+
