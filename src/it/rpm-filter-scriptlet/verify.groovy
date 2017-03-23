f0aef41197b4e46d13e3cd82998e3a3179f1e861
Ian Bondoc <chiknrice@gmail.com>
2017-03-24 02:19:12 +1300

    Add capability to use a scriptFile located in the classpath

--- src/it/rpm-filter-scriptlet/verify.groovy
+++ src/it/rpm-filter-scriptlet/verify.groovy
@@ -6,6 +6,9 @@ assert specFile.text.contains("echo \"installing rpm-filter-scriptlet\"")
 // Test if the post install <scriptFile> was filtered
 assert specFile.text.contains("PROJECT_NAME=rpm-filter-scriptlet")
 
+// Test if the pre remove <scriptFile> was filtered
+assert specFile.text.contains("echo \"Erasing version 1.0\"")
+
 // Test if the trigger script was filtered
 assert specFile.text.contains("echo \"a filtered install trigger for rpm-filter-scriptlet\"")
 
--- src/main/java/org/codehaus/mojo/rpm/Scriptlet.java
+++ src/main/java/org/codehaus/mojo/rpm/Scriptlet.java
@@ -59,7 +59,7 @@ public class Scriptlet
      *
      * @see #getScriptFile()
      */
-    private File scriptFile;
+    private String scriptFile;
 
     /**
      * Encoding of {@link #scriptFile}.
@@ -69,7 +69,7 @@ public class Scriptlet
     private String fileEncoding;
 
     /**
-     * The default encoding of the project, used if {@link fileEncoding} is not set.
+     * The default encoding of the project, used if {@link #fileEncoding} is not set.
      *
      * @see #getFileEncoding().
      */
@@ -141,7 +141,7 @@ public class Scriptlet
      *
      * @return Returns the {@link #scriptFile}.
      */
-    public File getScriptFile()
+    public String getScriptFile()
     {
         return this.scriptFile;
     }
@@ -149,7 +149,7 @@ public class Scriptlet
     /**
      * @param scriptFile The {@link #scriptFile} to set.
      */
-    public void setScriptFile( File scriptFile )
+    public void setScriptFile( String scriptFile )
     {
         this.scriptFile = scriptFile;
     }
@@ -214,13 +214,13 @@ public class Scriptlet
     protected final void write( final PrintWriter writer, final String directive, final List<FileUtils.FilterWrapper> filterWrappers)
         throws IOException
     {
-        if ( scriptFile != null && !scriptFile.exists() )
+        /*if ( scriptFile != null && !scriptFile.exists() )
         {
             throw new RuntimeException( "Invalid scriptlet declaration found - defined scriptFile does not exist: "
                 + scriptFile.getPath() );
-        }
+        }*/
 
-        if ( script != null || ( scriptFile != null && scriptFile.exists() ) || program != null )
+        if ( script != null || scriptFile != null || program != null )
         {
             writer.println();
             writer.println( buildScriptletLine( directive ) );
@@ -255,6 +255,27 @@ public class Scriptlet
         return builder.toString();
     }
 
+    private Reader getScriptReader(String path) throws FileNotFoundException, UnsupportedEncodingException {
+        String classpathPrefix = "classpath:";
+        Reader reader;
+        if (path.startsWith(classpathPrefix)) {
+            String classpathResource = path.substring(classpathPrefix.length());
+            InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(classpathResource);
+            if (inputStream == null) {
+                throw new RuntimeException("Invalid scriptlet declaration found - defined scriptFile does not exist: " + path);
+            }
+            reader = new InputStreamReader(inputStream);
+        } else {
+            File file = new File(path);
+            if (!file.exists()) {
+                throw new RuntimeException("Invalid scriptlet declaration found - defined scriptFile does not exist: " + path);
+            }
+            reader = fileEncoding != null ? new InputStreamReader( new FileInputStream( file ), fileEncoding )
+                    : new FileReader( scriptFile );
+        }
+        return reader;
+    }
+
     /**
      * Writes the content (either {@link #getScript()} or {@link #getScriptFile()}) to <i>writer</i>.
      *
@@ -269,11 +290,9 @@ public class Scriptlet
         {
             writer.println( script );
         }
-        else if ( scriptFile.exists() )
+        else if ( scriptFile != null )
         {
-            Reader reader =
-                    fileEncoding != null ? new InputStreamReader( new FileInputStream( scriptFile ), fileEncoding )
-                            : new FileReader( scriptFile );
+            Reader reader = getScriptReader(scriptFile);
             if(filter)
             {
                 for ( FileUtils.FilterWrapper filterWrapper : filterWrappers ) {
