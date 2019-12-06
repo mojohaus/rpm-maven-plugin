e353276d93dcce27d52bc24818ea50861f2be5d7
BO8979 <BO8979@W1971362.northamerica.cerner.net>
2019-12-06 12:39:10 -0600

    Revert "https://github.com/mojohaus/rpm-maven-plugin/issues/118"
    This reverts commit d2ced9c2e1ddf372b2be06a915cb3ff18fa9862c.

--- src/main/java/org/codehaus/mojo/rpm/SpecWriter.java
+++ src/main/java/org/codehaus/mojo/rpm/SpecWriter.java
@@ -18,9 +18,8 @@ package org.codehaus.mojo.rpm;
  * specific language governing permissions and limitations
  * under the License.
  */
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import java.io.*;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -239,8 +238,8 @@ final class SpecWriter
             scanner.setExcludes( null );
             scanner.scan();
 
-            final boolean noFiles = map.getSources().isEmpty() || (scanner.isEverythingIncluded() && links.isEmpty());
-            if ( noFiles && map.isDirectoryIncluded() && !map.isRecurseDirectories() )
+            if ( scanner.isEverythingIncluded() && links.isEmpty() && map.isDirectoryIncluded()
+                && !map.isRecurseDirectories() )
             {
                 log.debug( "writing attribute string for directory: " + destination );
                 spec.println( attrString + " \"" + destination + "\"" );
@@ -273,7 +272,7 @@ final class SpecWriter
                 }
 
                 // only list files if requested (directoryIncluded == false) or we have to
-                if ( !map.isDirectoryIncluded() || !noFiles )
+                if ( !( map.isDirectoryIncluded() && scanner.isEverythingIncluded() && links.isEmpty() && !map.isRecurseDirectories()) )
                 {
                     final String[] files = scanner.getIncludedFiles();
 
